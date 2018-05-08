6a099f9a42fc726584d8249b2ab658f1fcb0100c
Adonaï Elohim Exaucé <adonainangui03@gmail.com>
2018-05-08 17:04:19 +0000

    queue worker

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Mail\TestMail;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/check-queue', function () {
+    Mail::to('adonainangui03@gmail.com')->send(new TestMail());
+    return 'Working';
 });
